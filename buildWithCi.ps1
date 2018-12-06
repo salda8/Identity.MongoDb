2b3bb3946dbce2ef965740c2dd87e8decf008f4c
Vojtech Salovsky <v.salovsky@kendaxa.com>
2018-12-06 11:29:48 +0100

    Change ci script to push package, removed paket from tests

--- buildWithCi.ps1
+++ buildWithCi.ps1
@@ -1,3 +1,8 @@
 dotnet build -c Release
+dotnet pack -c Release -p:PackageVersion=1.0.1 IdentityMongoDb/IdentityMongoDb.csproj  -o ../
+
+nuget push *.nupkg -ApiKey pu8ntw4uin1t4g2s1tt7lm2e -Source https://ci.appveyor.com/nuget/salda8-0h4n5nbbutro/api/v2/package
+nuget push *.nupkg -ApiKey o3yk865auhn9olmkxtmn2v49 -Source https://www.myget.org/F/salda/api/v2/package	
+
 Invoke-WebRequest -Uri 'https://codecov.io/bash' -OutFile codecov.sh
 .\codecov.sh -f "coverage.xml"
